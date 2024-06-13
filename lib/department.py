a7d9d0323bc6088b6e02d08c2429e91d1f59e7ba
wathika-eng <wathika02@gmail.com>
2024-06-13 17:09:07 +0300

    chore: Update Department table creation and deletion

--- lib/department.py
+++ lib/department.py
@@ -3,6 +3,27 @@ from __init__ import CURSOR, CONN
 
 class Department:
 
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Department instances"""
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
